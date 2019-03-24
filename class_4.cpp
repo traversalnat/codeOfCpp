075ed2441a2307a23af71bf235c77ff2e16f155d
traversalnat <1914619958@qq.com>
2019-03-24 11:58:07 +0800

    第四章下部分

--- class_4.cpp
+++ class_4.cpp
@@ -72,7 +72,7 @@ namespace Complex {
 
 }
 
-// 容器类(包含若干元素的对象)
+// 容器类(包含若干元素的对象, 具体类的一种)
 namespace Container {
 	class Vector {
 		private:
@@ -170,8 +170,144 @@ namespace Container {
 	}
 };
 
+// 抽象类(分离接口和类)
+namespace Virtual {
+	class Container {
+		// Container 接口, Vector 更抽象的版本
+		public:
+			//virtual 表示虚函数, 虚函数 = 0表示纯虚函数
+			virtual double& operator[](int) = 0;
+			virtual int size() const = 0;
+			virtual ~Container() {}
+	};
+
+	// :public 派生自 的含义
+	class Vector_container : public Container {
+		// 命名空间与内部类重名, 在命名空间前加 ::
+		::Container::Vector v;
+		public:
+			Vector_container(int s) : v(s) {}
+			~Vector_container() {}
+
+			// override 表示显示覆盖, 如果父类没有对应的虚函数
+			// 将报错, 防止没有覆盖某些虚函数
+			double& operator[](int i) override {
+				return v[i];
+			}
+			int size() const override {
+				return v.size();
+			}
+
+			// 自定义成员函数
+			void swink() {
+				std::cout<<"Vector_container.swink\n";
+			}
+	};
+
+	// 接口的使用和java 类似
+	void use(Container& c) {
+		const int sz = c.size();
+		for (int i = 0; i != sz; i++) {
+			std::cout<< c[i] << " ";
+		}
+		std::cout<<std::endl;
+	}
+	int main() {
+		// 构造函数的使用, 依然可以使用 {} 直接赋值
+		Vector_container v {3};
+		for (int i = 0; i < 3; i++) {
+			v[i] = i;
+		}
+		use(v);
+
+		// 使用 dynamic_cast<>() 表示期望得到的对象
+		// 如果不是, 则返回nullptr, 即希望得到一个具体类的实例
+		Container* p = &v;
+		if (Vector_container* q = dynamic_cast<Vector_container*>(p)) {
+			q -> swink();
+		} else {
+			std::cout<<"Can't swink!\n";
+		}
+		return 0;
+	}
+};	
+
+// 拷贝和移动, 关于类的操作
+namespace Operators {
+	class Vector {
+		private:
+			double *elem;
+			int sz;
+		public:
+			Vector(int s) :elem {new double[s]}, sz {s} {
+
+			}
+
+			// 拷贝构造函数
+			Vector(const Vector& x)
+				: elem {new double[x.sz]}, sz {x.sz} {
+					for (int i = 0; i < x.sz; i++) {
+						elem[i] = x.elem[i];
+					}
+			}
+
+			// 移动函数
+			// 如何与拷贝构造函数区分开来??
+			// 可以使用 std::move(x), 如 Vector y(std::move(x))
+			// 来告诉编译器使用右值引用(操作等号右边的变量)
+			Vector(Vector&& x) : elem {x.elem}, sz {x.sz} {
+				x.elem = nullptr;
+				x.sz = 0;
+			}
+			
+			~Vector() {delete[] elem;}
+
+
+			double& operator[] (int i) {
+				if (i < 0 || i > sz) {
+					throw std::out_of_range("Vector::operator[]");
+				}
+				return elem[i];
+			}
+
+			// 赋值拷贝函数
+			Vector& operator=(const Vector& x) {
+				double *p = new double[x.sz];
+				for (int i = 0; i < x.sz; i++) {
+					p[i] = x.elem[i];
+				}
+				delete[] elem;
+				elem = p;
+				sz = x.sz;
+				return *this;
+			}
+
+			// 有些时候, 我们不希望用到拷贝或者移动构造函数, 或其它操作
+			// 可以使用 = delete; 表示抑制该操作
+			// 如
+			// Vector(Vector&) = delete;
+
+			int size() const {return sz;}
+	};
+
+
+
+	int main() {
+		Vector x(2);
+		x[0] = 1;
+		x[1] = 1;
+		Vector y(std::move(x));
+		y[0] = 2;
+		std::cout<<x.size()<<"\n";
+		return 0;
+	}
+
+};
+
 
 int main() {
 	Complex::main();
 	Container::main();
+	Virtual::main();
+	Operators::main();
 }
