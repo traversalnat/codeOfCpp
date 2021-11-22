289ce0d0a56602f81caf07d5cf828622ccd31eb7
traversalnat <1914619958@qq.com>
2021-11-22 14:34:41 +0800

    mem_logic: 实验类作为参数时的行为

--- mem_logic/mem_logic.cpp
+++ mem_logic/mem_logic.cpp
@@ -23,17 +23,33 @@ public:
 
       Data(const Data &d) : a {d.a}, name { to_string(name_index) + d.name} {
         name_index += 1;
-        cout << name << ": copied by copy constructor\n";
+        cout << d.name << ": copied by copy constructor to " + name + "\n";
+      }
+      
+      Data(Data &&d) : a {d.a}, name { d.name } {
+        cout << d.name << ": moved by move constructor to " + name + "\n";
+        d.a = -1;
+        d.name = "moved " + d.name;
       }
 
       ~Data() {
         cout << this->name << ": free by ~Data\n";
       }
 
-      Data& operator=(Data &d) {
+      Data& operator=(const Data &d) {
         this->a = d.a;
         this->name = "COPY= " + d.name;
-        cout << this->name << ": copied by operator=\n";
+        cout << d.name << ": copied by operator= to " + this->name + "\n";
+        return *this;
+      }
+
+      // move assignment
+      Data& operator=(Data &&d) {
+        this->a = d.a;
+        this->name = "MOVE= " + d.name;
+        cout << this->name << ": moved by operator=\n";
+        d.a = -1;
+        d.name = nullptr;
         return *this;
       }
  
@@ -50,29 +66,38 @@ void set(Data d) {
 }
 
 Data copy(Data d) {
-  // 实际上返回的是传入参数通过复制构造函数复制的副本
+  // 将传入参数视为 d , 传入参数 d 时会调用复制构造函数得到 d1
+  // 函数中的 d 实际上是 d1
   return d;
-  // 由于 return, 在此不会触发 d 的析构函数
+  // return d 实际上调用了 d1 的移动构造函数(若无，则调用复制构造函数), 返回的是 d2
 }
 
 class Mem_Logic : public Test {
 };
 
 TEST_F(Mem_Logic, mem_logic_test) {
+  // 总结: 
+  // 1. 类作为参数传入函数，实际上会复制一个副本（复制构造函数），在函数中对参数的操作对原变量无影响
+  // 2. 函数在返回一个类时，会尝试调用移动构造函数（若无则调用复制构造函数）得到返回类的副本并返回, 释放函数中所有不在作用域中的类
+  // 3. std::move 调用移动构造函数或移动赋值函数，若无则调用复制构造函数或复制赋值函数 (注意初始化类时一定调用的是构造函数, 不论使用 = 还是 {} )
   Data a {1, "a1"};
   cout << "\n\nset function\n";
   set(a);
-  // 由于没有变量接收 copy 返回值, 在此处会触发 copy 返回值的析构函数
+  // copy 复制了 a, 并通过移动构造函数将 a 的副本移动至 d2 (若无移动构造函数，则调动复制构造函数), 随后 a 的副本被释放, d2 由于离开作用域, 也被释放
   cout << "\n\ncopy function\n";
   copy(a);
 
   cout << "\n\ncopy2 function\n";
-  // 变量 b 通过复制构造函数获得 copy 返回值的副本
-  // = 号触发了复制构造函数
+  // copy 复制了 a, 并通过移动构造函数将 a 的副本移动至 b (即 b 就是 d2, 若无移动构造函数，则调动复制构造函数), 随后 a 的副本被释放
   auto b = copy(a);
 
-  cout << "\n\nfree function\n";
   ASSERT_EQ(1, a.a);
   ASSERT_EQ(1, b.a);
+
+  cout << "\n\nmove function\n";
+  // std::move 如果没有移动赋值函数, 会调用复制构造函数
+  auto b1 = std::move(b);
+  ASSERT_EQ(1, b1.a);
+  cout << "\n\nfree function\n";
 }
 
