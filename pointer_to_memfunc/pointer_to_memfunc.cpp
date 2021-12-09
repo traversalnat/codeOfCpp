4147e10b7d10e3a1cd6fd6bc354e2e7c8f4cc19a
traversalnat <1914619958@qq.com>
2021-12-09 20:37:42 +0800

    pointer_to_memfunc: 模拟 qt connect 中信号/槽机制, 但是无法处理参数

--- pointer_to_memfunc/pointer_to_memfunc.cpp
+++ pointer_to_memfunc/pointer_to_memfunc.cpp
@@ -12,27 +12,148 @@ using testing::Test;
 using std::string;
 using std::cout;
 using std::endl;
+using std::vector;
 
 class Pointer_To_Memfunc : public Test {
 };
 
-class Receiver {
+// 模板类SenderSlot无法直接保存, 需要使用基类
+class ISenderSlot {
   public:
-    Receiver(const string &str): name {str} {}
-    // 成员函数指针 typedef, 简化使用
-    typedef void (Receiver::*slot_ptr) (const string &);
-    void slot(const string &str) {
-      cout << "Receive: " << str << endl;
+    virtual void call() = 0;
+    virtual string getKey() = 0;
+};
+
+/**
+ * @brief 用于保存信号/槽
+ *
+ * @tparam S 信号类
+ * @tparam SIGNAL 信号函数
+ * @tparam T 槽类
+ * @tparam SLOT 槽函数
+ */
+template<typename S, typename SIGNAL, typename T, typename SLOT>
+class SenderSlot : public ISenderSlot {
+  // 用于保存 sender 和 slot 
+  public:
+    SenderSlot(S *sender, SIGNAL signal, T *receiver, SLOT slot) 
+      : sender {sender}, signal {signal}, receiver {receiver}, slot {slot} {}
+
+    /**
+     * @brief 调用receiver 的槽函数
+     */
+    void call() {
+      (receiver->*slot)();
+    }
+
+    /**
+     * @brief 返回 信号SIGNAL 的类型
+     *
+     * @return 
+     */
+    string getKey() {
+      string name {typeid(SIGNAL).name()};
+      return name;
+    }
+
+  private:
+    S *sender;
+    SIGNAL signal;
+    T *receiver;
+    SLOT slot;
+};
+
+// 单例处理中心
+// 存储 signal 和 slot 之间的关系, 在 signal 发起调用后自动调用 slot 函数
+class CENTER {
+  private:
+    CENTER() {}
+    CENTER(const CENTER &) = delete;
+    CENTER& operator=(const CENTER &) = delete;
+    // 单例模式
+    static CENTER *center; 
+    /**
+     * @brief 暂时使用 vector, 可更改为 map
+     */
+    vector<ISenderSlot *> senderSlots;
+  public:
+    ~CENTER() {}
+
+    static CENTER* getCenter() {
+      if (center == nullptr) {
+        center = new CENTER();
+      }
+
+      return center;
+    }
+
+    template <typename SIGNAL>
+    void call() {
+      /// 从 senderSlots 获取 SIGNAL 对应的所有 SenderSlot, 并调用 call()
+      string key {typeid(SIGNAL).name()};
+      for (auto ss : senderSlots) {
+        if (ss->getKey() == key) {
+          ss->call();
+        }
+      }
+    }
+
+    /**
+     * @brief 存储 SenderSlot 用于调用
+     *
+     * @param ss
+     */
+    void insert(ISenderSlot *ss) {
+      senderSlots.push_back(ss);
+    }
+
+};
+
+/**
+ * @brief 单例模式必须在外部初始化 center
+ */
+CENTER* CENTER::center = nullptr;
+
+class Sender {
+  public:
+    Sender(const string &str): name {str} {}
+    // 还没想好参数怎么处理
+    void signal() {}
+
+    /**
+     * @brief 模拟 emit signal 
+     */
+    void trigger() {
+      cout << name << " has sended!" << endl;
+      typedef void (Sender::*signal_ptr)();
+      CENTER::getCenter()->call<signal_ptr>();
     }
   private:
     string name;
 };
 
+class Receiver {
+  public:
+    Receiver(const string &str): name {str} {}
+    void slot() {
+      cout << name << " has received" << endl;
+    }
+  private:
+    string name;
+};
+
+template<typename S, typename SIGNAL, typename T, typename SLOT>
+void connect(S *sender, SIGNAL signal, T *receiver, SLOT slot) {
+  SenderSlot<S, SIGNAL, T, SLOT> *ss = new SenderSlot<S, SIGNAL, T, SLOT> {sender, signal, receiver, slot};
+  CENTER::getCenter()->insert(ss);
+}
+
 TEST_F(Pointer_To_Memfunc, pointer_to_memfunc_test) {
   Receiver *r {new Receiver {"Receiver"}};
-  // 由于成员函数指针的类型转换限制, 类似qt connect() 的实现需要更多信息
-  Receiver::slot_ptr slot_ptr = &Receiver::slot;
-  // () 的优先级大于 *
-  (r->*slot_ptr)("hello");
+  Sender *s {new Sender {"Sender"}};
+  // 借助模板的自动推导, 不用设定类型
+  connect(s, &Sender::signal, r, &Receiver::slot);
+  // 模拟 emit signal
+  s->trigger();
 }
 
